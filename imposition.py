de62712059c4504ef51e216d9d2435869db67a04
pennylescroche <thescroche@gmail.com>
2023-06-23 08:49:46 -0400

    added loop, fixed result count

--- imposition.py
+++ imposition.py
@@ -58,105 +58,124 @@ spell_size = 0
 path_size = 0
 imp_size = 0
 rolls = 0
-while not(dice):
-    print("Would you like to use the old or new imposition dice system?")
-    type = input()
-    if type.lower() == "old" or type.lower() == "new":
-        dice = True
-    else:
-        print("invalid input, please try again")
-while not (spell):
-    print("What is the size of the spell die(4,6,8,10,12)?")
-    spell_size = input()
-    if int(spell_size) == 4 or int(spell_size) == 6 or int(spell_size) == 8 or int(spell_size) == 10 or int(spell_size) == 12:
-        spell = True
-    else:
-        print("Invalid input, plese try again")
-while not (path):
-    print("What is the size of the path die(4,6,8,10,12)?")
-    path_size = input()
-    if int(path_size) == 4 or int(path_size) == 6 or int(path_size) == 8 or int(path_size) == 10 or int(path_size) == 12:
-        path = True
-    else:
-        print("Invalid input, plese try again")        
-while not (imp):
-    print("What is the size of the imposition die(4,6,8,10,12)?")
-    imp_size = input()
-    if int(imp_size) == 4 or int(imp_size) == 6 or int(imp_size) == 8 or int(imp_size) == 10 or int(imp_size) == 12:
-        imp = True
-    else:
-        print("Invalid input, plese try again")        
-while not roll:
-    print("How many Rolls would you like to make?")
-    rolls = input()
-    if int(rolls) > 0:
-        roll = True
-        rolls = int(rolls)
-    else:
-        print("Invalid input, please try again")
-
-results = []
-output = []
-
-#print("rolled "+str(rolls)+" dice")
-#print(results.count())
-success = 0
-partial = 0
-failure = 0
-if type.lower()=="old":
-    for x in range(rolls):
-        result = Old_Result(int(spell_size),int(path_size),int(imp_size))
-        results.append(result)  
-    output.append(["Spell","Path","Imposition","Die Result","Result"])
-    for x in range(len(results)):
-        output.append([results[x].spell,results[x].path,results[x].imp,results[x].die_result,results[x].result])
-    for x in range(len(output)):
-        if output[x][4]=="success":
-            success+=1
-        elif output[x][4] == "partial success":
-            partial+=1
+loop = True
+while(loop):   
+    while not(dice):
+        print("Would you like to use the old or new imposition dice system?")
+        type = input()
+        if type.lower() == "old" or type.lower() == "new":
+            dice = True
         else:
-            failure+=1
-        print(output[x])
-    print("For "+str(rolls)+" rolls in the old Imposition Dice system with a d"+str(spell_size)+" Spell Die, a d"+str(path_size)+" Path Die, and a d"+str(imp_size)+" Imposition Die the results are:")
-    print("Successes: "+str(success)+", Partial Successes: "+str(partial)+", Failures: "+str(failure))
-    print("Output to file(y/n)?")
-    out = input()
-    if out == "y":
-        a = datetime.now()
-        filename = "imposition_new_"+str(a.strftime("%Y-%m-%dT%H%M")+".csv")
-        f = open(filename,"w")
-        f.write("Spell("+str(spell_size)+"), Path("+str(path_size)+"), Imposition("+str(imp_size)+"), Die Result, Result\n")
-        for x in range(len(results)):
-            f.write(str(results[x].spell)+","+str(results[x].path)+","+str(results[x].imp)+","+str(results[x].die_result)+","+str(results[x].result)+"\n")
-        f.close()
-        print("File "+filename+" has been created")
-else:
-    for x in range(rolls):
-        result = New_Result(int(spell_size),int(path_size),int(imp_size))
-        results.append(result)
-    output.append(["Spell","Path","Imposition","Result"])
-    for x in range(len(results)):
-        output.append([results[x].spell,results[x].path,results[x].imp,results[x].result])
-    for x in range(len(output)):
-        if output[x][3]=="success":
-            success+=1
-        elif output[x][3] == "partial success":
-            partial+=1
+            print("invalid input, please try again")
+    while not (spell):
+        print("What is the size of the spell die(4,6,8,10,12)?")
+        spell_size = input()
+        if int(spell_size) == 4 or int(spell_size) == 6 or int(spell_size) == 8 or int(spell_size) == 10 or int(spell_size) == 12:
+            spell = True
         else:
-            failure+=1
-        print(output[x])
-    print("For "+str(rolls)+" rolls in the new Impositions Dice system with a d"+str(spell_size)+" Spell Die, a d"+str(path_size)+" Path Die, and a d"+str(imp_size)+" Imposition Die the results are:")
-    print("Successes: "+str(success)+", Partial Successes: "+str(partial)+", Failures: "+str(failure))
-    print("Output to file(y/n)?")
-    out = input()
-    if out == "y":
-        a = datetime.now()
-        filename = "imposition_new_"+str(a.strftime("%Y-%m-%dT%H%M")+".csv")
-        f = open(filename,"w")
-        f.write("Spell("+str(spell_size)+"), Path("+str(path_size)+"), Imposition("+str(imp_size)+"), Result\n")
+            print("Invalid input, plese try again")
+    while not (path):
+        print("What is the size of the path die(4,6,8,10,12)?")
+        path_size = input()
+        if int(path_size) == 4 or int(path_size) == 6 or int(path_size) == 8 or int(path_size) == 10 or int(path_size) == 12:
+            path = True
+        else:
+            print("Invalid input, plese try again")        
+    while not (imp):
+        print("What is the size of the imposition die(4,6,8,10,12)?")
+        imp_size = input()
+        if int(imp_size) == 4 or int(imp_size) == 6 or int(imp_size) == 8 or int(imp_size) == 10 or int(imp_size) == 12:
+            imp = True
+        else:
+            print("Invalid input, plese try again")        
+    while not roll:
+        print("How many Rolls would you like to make?")
+        rolls = input()
+        if int(rolls) > 0:
+            roll = True
+            rolls = int(rolls)
+        else:
+            print("Invalid input, please try again")
+
+    results = []
+    output = []
+
+    #print("rolled "+str(rolls)+" dice")
+    #print(results.count())
+    success = 0
+    partial = 0
+    failure = 0
+    if type.lower()=="old":
+        for x in range(rolls):
+            result = Old_Result(int(spell_size),int(path_size),int(imp_size))
+            results.append(result)  
+        output.append(["Spell","Path","Imposition","Die Result","Result"])
         for x in range(len(results)):
-            f.write(str(results[x].spell)+","+str(results[x].path)+","+str(results[x].imp)+","+str(results[x].result)+"\n")
-        f.close()
-        print("File "+filename+" has been created")
-    
+            output.append([results[x].spell,results[x].path,results[x].imp,results[x].die_result,results[x].result])
+        for x in range(len(output)):
+            if output[x][4]=="success":
+                success+=1
+            elif output[x][4] == "partial success":
+                partial+=1
+            else:
+                if x != 0:
+                    failure+=1
+            print(output[x])
+        print("For "+str(rolls)+" rolls in the old Imposition Dice system with a d"+str(spell_size)+" Spell Die, a d"+str(path_size)+" Path Die, and a d"+str(imp_size)+" Imposition Die the results are:")
+        print("Successes: "+str(success)+", Partial Successes: "+str(partial)+", Failures: "+str(failure))
+        print("Output to file(y/n)?")
+        out = input()
+        if out == "y":
+            a = datetime.now()
+            filename = "imposition_new_"+str(a.strftime("%Y-%m-%dT%H%M")+".csv")
+            f = open(filename,"w")
+            f.write("Spell("+str(spell_size)+"), Path("+str(path_size)+"), Imposition("+str(imp_size)+"), Die Result, Result\n")
+            for x in range(len(results)):
+                f.write(str(results[x].spell)+","+str(results[x].path)+","+str(results[x].imp)+","+str(results[x].die_result)+","+str(results[x].result)+"\n")
+            f.close()
+            print("File "+filename+" has been created")
+    else:
+        for x in range(rolls):
+            result = New_Result(int(spell_size),int(path_size),int(imp_size))
+            results.append(result)
+        output.append(["Spell","Path","Imposition","Result"])
+        for x in range(len(results)):
+            output.append([results[x].spell,results[x].path,results[x].imp,results[x].result])
+        for x in range(len(output)):
+            if output[x][3]=="success":
+                success+=1
+            elif output[x][3] == "partial success":
+                partial+=1
+            else:
+                if x != 0:
+                    failure+=1
+            print(output[x])
+        print("For "+str(rolls)+" rolls in the new Impositions Dice system with a d"+str(spell_size)+" Spell Die, a d"+str(path_size)+" Path Die, and a d"+str(imp_size)+" Imposition Die the results are:")
+        print("Successes: "+str(success)+", Partial Successes: "+str(partial)+", Failures: "+str(failure))
+        print("Output to file(y/n)?")
+        out = input()
+        if out == "y":
+            a = datetime.now()
+            filename = "imposition_new_"+str(a.strftime("%Y-%m-%dT%H%M")+".csv")
+            f = open(filename,"w")
+            f.write("Spell("+str(spell_size)+"), Path("+str(path_size)+"), Imposition("+str(imp_size)+"), Result\n")
+            for x in range(len(results)):
+                f.write(str(results[x].spell)+","+str(results[x].path)+","+str(results[x].imp)+","+str(results[x].result)+"\n")
+            f.close()
+            print("File "+filename+" has been created")
+    a = True
+    while(a):
+        print("Run Again(y/n)?")
+        b = input()
+        if(b == "n"):
+            a = False
+            loop = False
+        elif b == "y":
+            a = False
+            dice = False
+            spell = False
+            path = False
+            imp = False
+            roll = False
+        else:
+            print("Invalid Input. Please try again")
